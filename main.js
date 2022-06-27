12cad81db197eb6367487e1cc3f67d711295643b
Larimar Fondeur <larimarfondeur@gmail.com>
2022-06-27 18:02:05 -0400

    added styles

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const pokemon = ['charmander', 'bulbasaur', 'squirtle']
 const pokemonIDs = ['001', '002', '007']
+const containerDiv = document.querySelector('#container')
 
 // DOM = Document Object Model 
 // loop over every ID
@@ -10,13 +11,11 @@ const pokemonIDs = ['001', '002', '007']
 pokemonIDs.map((id) => {
     let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
     console.log('img url', imgUrl)
-    let div = document.createElement('div') // adding a dive to house the img element 
+    let div = document.createElement('div') // adding a div to house the img element 
     div.setAttribute('class', 'pokemon-card') // setting the class of the div 
     let img = document.createElement('img') // creating the img element itself
     img.src = imgUrl
-    img.setAttribute('height', '100px')
-    img.setAttribute('width', '100px') // append the img element to the div
     div.append(img) 
-    document.body.append(div)
+    containerDiv.append(div)
 
 })
