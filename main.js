64e86bbfda273bb0c51516b9c2cb942c1a35080f
Larimar Fondeur <larimarfondeur@gmail.com>
2022-06-28 13:30:43 -0400

    added roster

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 
 const pokemon = [
-    {name: 'bulbasaur', id: '001'},
-    {name: 'charmander', id: '004'},
-    {name: 'squirtle', id: '007'}
+    {name: 'Bulbasaur', id: '001'},
+    {name: 'Charmander', id: '004'},
+    {name: 'Squirtle', id: '007'}
 ]
 
 
 const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+const rosterDiv = document.querySelector('#roster')
+
+newBtn.addEventListener('click', () => {
+    let num = prompt('ENTER A POKEMON NUMBER')
+    console.log('Number entered', num)
+})
 
 // DOM = Document Object Model 
 // loop over every ID
@@ -22,8 +29,18 @@ pokemon.map((element, index) => {
     h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card') // setting the class of the div 
     let img = document.createElement('img') // creating the img element itself
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio')
+    let source = document.createElement('source')
+    source.setAttribute('src', audioUrl)
+    source.setAttribute('type', 'audio/mpeg')
+    audio.append(source)
+    div.addEventListener('click', () => {
+        console.log('audio', audioUrl)
+        audio.play()
+    })
     img.src = imgUrl
-    div.append(img, h3) // append the img element to the div
+    div.append(img, h3, audio) // append the img element to the div
     containerDiv.append(div)
 })
 
