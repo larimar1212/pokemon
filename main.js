a8e77413a502b3198ccc2ff74f36799f5d239cc6
Larimar Fondeur <larimarfondeur@gmail.com>
2022-06-28 11:24:00 -0400

    Added javascript file

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const pokemon = ['charmander', 'bulbasaur', 'squirtle']
-const pokemonIDs = ['001', '002', '007']
+
+const pokemon = [
+    {name: 'bulbasaur', id: '001'},
+    {name: 'charmander', id: '004'},
+    {name: 'squirtle', id: '007'}
+]
+
+
 const containerDiv = document.querySelector('#container')
 
 // DOM = Document Object Model 
@@ -8,14 +14,17 @@ const containerDiv = document.querySelector('#container')
 // set the HTML element values
 // append HTML element to the DOM 
 
-pokemonIDs.map((id) => {
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log('img url', imgUrl)
+pokemon.map((element, index) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    console.log(element)
     let div = document.createElement('div') // adding a div to house the img element 
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card') // setting the class of the div 
     let img = document.createElement('img') // creating the img element itself
     img.src = imgUrl
-    div.append(img) 
+    div.append(img, h3) // append the img element to the div
     containerDiv.append(div)
-
 })
+
+
